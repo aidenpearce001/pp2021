86fd153cdabf74e0901364ed47f64d27edd119ff
aidenpearce001 <aidenpeacewd01@gmail.com>
2021-04-14 11:07:51 +0700

    'labwork5'

--- labwork5/Main.py
+++ labwork5/Main.py
@@ -1,19 +1,22 @@
+import zipfile
+
+
 from Manage_Input import Input_Manage
 from Manage_Output import Output_Manage
 
 class domains:
-	def __init__(self):
+    def __init__(self):
+        pass
 
-		pass 
 
-	def ex4(self,):
+    def ex5(self,):
 
-		obj = Input_Manage('' ,'', '' ,  0)
-		obj.add('Aiden', 'bi10-124', '2001', (('Advanced Programming ','17.5'),('French ','15.25'), ('Singal ','14.25'), ('Basic Database','17.25')) )
-		obj.add('Na', 'bi10-010', '2001', (('Advanced Programming','16.75'),('Chemistry','12.25'), ('Web Dev', '19')) )
+        obj = Input_Manage('' ,'', '' ,  0)
+        obj.add('Aiden', 'bi10-124', '2001', (('Advanced Programming ','17.5'),('French ','15.25'), ('Singal ','14.25'), ('Basic Database','17.25')) )
+        obj.add('Na', 'bi10-010', '2001', (('Advanced Programming','16.75'),('Chemistry','12.25'), ('Web Dev', '19')) )
 
-		display = Output_Manage(None)
-		display.display(obj.students_list)
+        obj.export(obj.students_list)
+        obj.running()
 
 if __name__ == "__main__":
     main = domains() 
--- labwork5/Manage_Input.py
+++ labwork5/Manage_Input.py
@@ -1,17 +1,64 @@
 class Input_Manage:
-    def __init__(self, name, sid, dob, course, students_list=[]):
+    
+    def __init__(self, name, sid, dob, course):
         self.name   = name
         self.sid    = sid
         self.dob    = dob
         self.course = course
-        self.students_list = students_list
-         
-    def add(self, name, sid, dob, course ): 
+        self.students_list = []
+    
+    def add(self, name, sid, dob, course ):
         ob = Input_Manage(name, sid, dob, course)
         self.students_list.append(ob)
         
-    def delete(self, name):
-        for i in range(ls.__len__()):
-            if name in ls[i].name:
-                print(f"Student name: {name} will be kick out")
-                del ls[i]
+    def file_compress(self, inp_file_names, output_name):
+
+        compression = zipfile.ZIP_DEFLATED
+
+        print(f' *** out_zip_file is - {output_name}')
+        zf = zipfile.ZipFile(output_name, mode="w")
+
+        try:
+            for file_to_write in inp_file_names:
+                print(f' *** Processing file {file_to_write}')
+                zf.write(file_to_write, file_to_write, compress_type=compression)
+
+        except FileNotFoundError as e:
+            print(f' *** Exception occurred during zip process - {e}')
+
+        zf.close()
+        
+    def compress(self):
+        list_files = ['students.txt','courses.txt','marks.txt']
+        
+        self.file_compress(list_files, 'students.dat')
+        
+    def decompress(self):
+        try:
+            with zipfile.ZipFile("students.dat","r") as zip_ref:
+                zip_ref.extractall("decompress")
+        except Exeption as E:
+            print('FILE students.dat not found')
+            print('ERROR :',E)
+        
+        
+    def export(self,infos):
+        for content in infos:
+            with open('students.txt','a+') as f:
+                f.write('Name :'+content.name+'\n')
+                f.write('  Student-ID: '+content.sid+'\n')
+                f.write('  Date Of Birth'+content.dob+'\n')
+                
+            with open('courses.txt','a+') as f1:
+                f1.write('Name :'+content.name+'\n')
+                for _ in content.course:
+                    f1.write('  Course:'+_[0]+'\n')
+                
+            with open('marks.txt','a+') as f2:
+                f2.write('Name :'+content.name+'\n')
+                for _ in content.course:
+                    f2.write(f'  {_[0]}:'+_[1]+'\n')
+                    
+    def running(self):
+        self.compress
+        self.decompress
